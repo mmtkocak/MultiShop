0d0756334f86b5413f661587046cf30cc24755e4
mmtkocak <mehmet-kocak@outlook.com.tr>
2024-04-03 15:48:19 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Settings/DatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.Catalog.Settings
+{
+    public class DatabaseSettings : IDatabaseSettings
+    {
+        public string CategoryColletionName { get; set; }
+        public string ProductColletionName { get; set; }
+        public string ProductDetailColletionName { get; set; }
+        public string ProductImageColletionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
