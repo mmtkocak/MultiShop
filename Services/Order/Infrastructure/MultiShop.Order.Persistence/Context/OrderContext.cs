9dfb796edbb59fd2bdaec00b668bc9dc9e41a68a
mmtkocak <mehmet-kocak@outlook.com.tr>
2024-04-19 16:19:47 +0300

    Identity Mikroservisi ve Json Web Token

--- Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
+++ Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MultiShop.Order.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MultiShop.Order.Persistence.Context
 {
@@ -12,7 +7,7 @@ namespace MultiShop.Order.Persistence.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=....; initial Catalog=MultiShopOrderDb; integrated Security=true");
+            optionsBuilder.UseSqlServer("Server=localhost,1440; initial Catalog=MultiShopOrderDb; User=sa; Password=123456Aa*");
         }
 
 
