1de1c5f50a525e581802c6d3ee0b2acc9f23e514
MEHMET KOÇAK <mehmet-kocak@outlook.com.tr>
2024-10-02 21:47:21 +0300

    Onion Architecture & CQRS Mediator Tasarım Desenleri ve Order Mikroservisi

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/CreateAddressCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class CreateAddressCommandHandler
+    {
+        private readonly IRepository<Address> _repository;
+        public CreateAddressCommandHandler(IRepository<Address> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Handle(CreateAddressCommand createAddressCommand)
+        {
+            await _repository.CreateAsync(new Address
+            {
+                City = createAddressCommand.City,
+                Detail1 = createAddressCommand.Detail1,
+                District = createAddressCommand.District,
+                UserId = createAddressCommand.UserId,
+                Country = createAddressCommand.Country,
+                Description = createAddressCommand.Description,
+                Detail2 = createAddressCommand.Detail2,
+                Email = createAddressCommand.Email,
+                Name = createAddressCommand.Name,
+                Phone = createAddressCommand.Phone,
+                Surname = createAddressCommand.Surname,
+                ZipCode = createAddressCommand.ZipCode
+            });
+        }
+    }
+}
