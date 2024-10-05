01af5210f1113ddf5e5b52223dd78a861ee029a5
MEHMET KOÇAK <mehmet-kocak@outlook.com.tr>
2024-10-05 16:31:56 +0300

    Kullanıcı Arayüzü Layout

---
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _FeatureComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
