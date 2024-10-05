320b886dcbf7c7dadb4b267bccca298fbf2d2ba8
MEHMET KOÇAK <mehmet-kocak@outlook.com.tr>
2024-10-05 22:00:16 +0300

    Ürün Sayfası

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProdutDetailInformationComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponents
+{
+    public class _ProdutDetailInformationComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProdutDetailReviewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponents
+{
+    public class _ProdutDetailReviewComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
