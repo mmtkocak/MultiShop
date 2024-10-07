69f4d76139545958f68bc105dc836273ca19620f
MEHMET KOÇAK <mehmet-kocak@outlook.com.tr>
2024-10-07 17:10:32 +0300

    Admin Kategori İşlemleri

--- Frontends/MultiShop.WebUI/Program.cs
+++ Frontends/MultiShop.WebUI/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddHttpClient();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -19,6 +19,11 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
 
 app.MapControllerRoute(
     name: "default",
