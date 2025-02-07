e852e6893698005ccf51c821c64e8cdbb379dd75
MEHMET KOÇAK <mehmet-kocak@outlook.com.tr>
2025-02-07 16:13:34 +0300

    ...

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/ProductDetailController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ProductDetailDtos;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class ProductDetailController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductDetailController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+       
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateProductDetail(string id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/ProductDetails/GetProductDetailByProductId?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateProductDetailDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateProductDetail(UpdateProductDetailDto updateProductDetailDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateProductDetailDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7070/api/ProductDetails/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("ProductListWithCategory", "Product");
+            }
+            return View();
+        }
+    }
+}
--- Services/Catalog/MultiShop.Catalog/Controllers/ProductDetailsController.cs
+++ Services/Catalog/MultiShop.Catalog/Controllers/ProductDetailsController.cs
@@ -29,7 +29,7 @@ namespace MultiShop.Catalog.Controllers
             return Ok(values);
         }
 
-        [HttpGet("GetProductDetailByProductId/{id}")]
+        [HttpGet("GetProductDetailByProductId")]
         public async Task<IActionResult> GetProductDetailByProductId(string id)
         {
             var values = await _productDetailService.GetByProductIdProductDetailAsync(id);
